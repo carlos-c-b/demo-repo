0bae51fa1e7ac9c35e8e433233e6005823a6da52
karlovv <carloscbc2004@gmail.com>
2024-07-26 18:44:14 +0200

    Added ExampleBox

---
+++ src/java/ExampleBox.java
@@ -0,0 +1,8 @@
+import javax.swing.Box;
+import java.awt.Dimension;
+
+public class ExampleBox {
+	public static void main(String[] args) {
+		Box.Filler myFiller = new Box.Filler(new Dimension(0,0), new Dimension(0,0), new Dimension(0,0));
+	}
+}
