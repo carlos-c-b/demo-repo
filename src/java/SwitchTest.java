cf99354329e2a70c9a2c1c72a8b1862c55d69206
karlovv <carloscbc2004@gmail.com>
2024-08-01 19:29:53 +0200

    Added SwitchTest.java

---
+++ src/java/SwitchTest.java
@@ -0,0 +1,12 @@
+public class SwitchTest {
+	public static void main(String[] args) {
+		int n = 10;
+
+		switch(n) {
+			case 1: case 2:
+			case 10:
+				System.out.println(10);
+				break;
+		}
+	}
+}
